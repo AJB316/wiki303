cb4a57052af942fd434aed3d808a73b0d03a1b18
AJB316 <a.bell568@montytech.net>
2024-12-12 13:56:07 -0500

    stuff
    added stuf

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
 const p = document.getElementById("txt")
 let article = ""
-let headersArray = []
 //todo: try and see if you can use the history api to let you go back to previous pages
 
 async function changeArticle(docName){ //thank you stack overflow for letting me skid this code
-const text = await fetch(`/wiki303/articles/${docName}.txt`)
+const text = await fetch(`./articles/${docName}.txt`)
 .then(response => response.text())
 .then(text => {
   // Do something with the text content
@@ -22,6 +21,7 @@ function applyArticle(text){ //this func applies text w markup
   text = text.replaceAll("\n","<br>")
 //const links = text.match(/\[.*\|.*\]/g)
 const links = text.match(/\[.+?\]/g) // /\[.*\|.*\]/g <- this is the one i was trying to use beforehand, thank you Owen Stephens on Google Groups for the one i'm using that works now
+const headers =  text.match(/\#.+/g)
 //console.log(links)
 for(const match in links){
   const split = links[match].split("|")
@@ -29,22 +29,21 @@ for(const match in links){
   text = text.replaceAll(links[match],`<i class="link" onclick="changeArticle('${split[1].substring(0,split[1].length-1)}')">${split[0].substring(1)}</i>`)
 }
 
-text = text.replaceAll(/\=\=\=.+?\=\=\=/g,"penguin")
+for(const match in headers){
+  text = text.replace(headers[match],`<h1>${headers[match].replace("# ","")}</h1>`)
+  console.log(headers)
+}
 
 
 
 if(text.match(/\!PICREL\=\".+?\"/)){ //articles should only have 1 picrel
   const picrel = text.match(/\!PICREL\=\".+?\"/)[0].substring(9,text.match(/\!PICREL\=\".+?\"/)[0].length - 1)
-  document.getElementById("picrel").src = "/wiki303" + picrel
+  document.getElementById("picrel").src = "./" + picrel
   text = text.replace(text.match(/\!PICREL\=\".+?\"/g),"") //sfhsu
 }
 
-const headers = text.match(/\=\=.*\=\=/)
-for(const header in headers){
-
-}
 if(text.match(/\!DESC\=.*/)){ //articles should only have 1 desc
-  const desc = text.match(/\!DESC\=.*/)[0].substring(6,text.match(/\!DESC\=.*/)[0].length - 1)
+  const desc = text.match(/\!DESC\=.*/)[0].substring(6,text.match(/\!DESC\=.*/)[0].length)
   document.getElementById("desc").innerHTML = desc
   text = text.replace(text.match(/\!DESC\=.*/g),"")
 }
