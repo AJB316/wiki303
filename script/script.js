bb025711656ad104528711853f192812f8c0a0aa
AJB316 <a.bell568@montytech.net>
2025-01-13 12:34:15 -0500

    YES
    previous commit fixed header fuckery now it applies to innerHTML and not innerText

--- script/script.js
+++ script/script.js
@@ -71,7 +71,7 @@ if(articleName==="articles"){
   }
 }
 
-p.innerText = text //supposed to be innerHTML this is for debugging
+p.innerHTML = text //supposed to be innerHTML innerText is for debugging
 }
 
 changeArticle("markup")
