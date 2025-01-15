133f60c6ff6a72339c357943f25984efb15466f9
AJB316 <a.bell568@montytech.net>
2025-01-15 09:06:21 -0500

    search history
    implemented History API and also URLSearchParams to share pages and use the browser arrows to switch between them

--- script/script.js
+++ script/script.js
@@ -2,7 +2,10 @@ const p = document.getElementById("txt")
 let article = ""
 let articleName = ""
 let allArticles = []
+let firstTime = true; //Holy spaghetti. I'm using this for the History API, just to not log the first page loaded (because the first page loaded isn't actually shown to the user, it can be confusing to them)
 //todo: try and see if you can use the history api to let you go back to previous pages
+const url = new URL(window.location)
+const urlSearchParams = new URLSearchParams(url.search)
 
 async function changeArticle(docName){ //thank you stack overflow for letting me skid this code
 const txt = await fetch(`./articles/${docName}.txt`)
@@ -22,7 +25,6 @@ applyArticle(text)
 
 function rudimentarySearch(text){
   let matches = []
-  console.log("commencing fent retrieval protocol")
   for(let i=0;i<allArticles.length;i++){
     if(allArticles[i].toLowerCase().includes(text.toLowerCase())){
       console.log(allArticles[i] + " skibidi")
@@ -44,7 +46,7 @@ for(const match in links){
   if(split.length > 1){
   text = text.replaceAll(links[match],`<i class="link" onclick="changeArticle('${split[1].substring(0,split[1].length-1)}')">${split[0].substring(1)}</i>`)
   } else {
-    text = text.replaceAll(links[match],`<i class="link" onclick="changeArticle('${rudimentarySearch(links[match].substring(1,links[match].length-1))}')">${links[match]}</i>`)
+    text = text.replaceAll(links[match],`<i class="link" onclick="changeArticle('${rudimentarySearch(links[match].substring(1,links[match].length-1))}')">${links[match].substring(1,links[match].length-1)}</i>`)
     console.log(links[match]+"sigma")
   }
 }
@@ -85,12 +87,28 @@ if(articleName==="articles"){
     //console.log(match)
     allArticles.push(things[match])
     text = text.replace(things[match],`<i class="link" onclick="changeArticle('${things[match].substring(0,things[match].length)}')">${things[match]}</i>`)
-   
+
   }
 }
 
 p.innerHTML = text //supposed to be innerHTML innerText is for debugging
+
+
+if(firstTime){
+  firstTime = false
+} else {
+  history.pushState(articleName,"",`/?article=${articleName}`)
 }
 
+}
+
+
+window.addEventListener("popstate",function(event){
+  firstTime = true //this val was originally just to prevent the articles page from being pushed to the history, but i've found using pushState on popstate makes it so you can't go forward
+  changeArticle(event.state)
+  //console.log(event.state+" go go go")
+})
+
 changeArticle("articles")
-changeArticle("markup")
+changeArticle(urlSearchParams.get("article") || "markup")
+
